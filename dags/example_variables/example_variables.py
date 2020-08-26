f8cecac4c6bcdbf37c055067a28ce9ccbd1c3dee
joaopamaral <joaopamaral@users.noreply.github.com>
2020-08-25 22:30:31 -0300

    commit docker example

--- dags/example_variables/example_variables.py
+++ dags/example_variables/example_variables.py
@@ -20,9 +20,9 @@ dag = DAG('example_variables',
 
 # Config variables
 ## Common
-# var1 = "value1"
-# var2 = [1, 2, 3]
-# var3 = {'k': 'value3'}
+var1 = "value1"
+var2 = [1, 2, 3]
+var3 = {'k': 'value3'}
 
 ## 3 DB connections called
 # var1 = Variable.get("var1")
@@ -30,10 +30,10 @@ dag = DAG('example_variables',
 # var3 = Variable.get("var3")
 
 ## Recommended way
-dag_config = Variable.get("example_variables_config", deserialize_json=True)
-var1 = dag_config["var1"]
-var2 = dag_config["var2"]
-var3 = dag_config["var3"]
+# dag_config = Variable.get("example_variables_config", deserialize_json=True)
+# var1 = dag_config["var1"]
+# var2 = dag_config["var2"]
+# var3 = dag_config["var3"]
 
 start = DummyOperator(
     task_id="start",
